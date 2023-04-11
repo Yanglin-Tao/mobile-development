ef68cff1818ec8854bf753c22393fb2432be2b40
Mitchellzhou1 <mitche.zhou1@gmail.com>
2023-04-11 10:55:53 -0400

    added boss and some animations

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Start(){
+    }
+
+    void Update(){
+
+    }
+}
