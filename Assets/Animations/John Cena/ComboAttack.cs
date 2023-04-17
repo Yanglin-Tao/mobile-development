693682980d7ba8815e02e66b1b31698ab50e7dfe
Suning Yao <sy2940@nyu.edu>
2023-04-17 13:02:59 -0400

    Rearrange order to allow melee hit1 during walking

--- Assets/Animations/John Cena/ComboAttack.cs
+++ Assets/Animations/John Cena/ComboAttack.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+
 public class ComboAttack : MonoBehaviour
 {
     private Animator anim;
@@ -11,7 +11,7 @@ public class ComboAttack : MonoBehaviour
     public static int noOfClicks = 0;
     float lastClickedTime = 0;
     float maxComboDelay = 1f;
- 
+
     private void Start()
     {
         anim = GetComponent<Animator>();
@@ -19,6 +19,15 @@ public class ComboAttack : MonoBehaviour
     }
     void Update()
     {
+        // Rearranges the order of the logic to check for mouse input first,
+        // then handle the animations and cooldown time.
+        // This should allow the player to trigger melee attacks while walking
+        // without the animations being overridden.
+        // Check for mouse input
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ComboSystem();
+        }
 
         if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.7f && anim.GetCurrentAnimatorStateInfo(0).IsName("melee1"))
         {
@@ -33,25 +42,19 @@ public class ComboAttack : MonoBehaviour
             anim.SetBool("hit3", false);
             noOfClicks = 0;
         }
- 
- 
+
         if (Time.time - lastClickedTime > maxComboDelay)
         {
             noOfClicks = 0;
         }
- 
+
         //cooldown time
         if (Time.time > nextFireTime)
         {
-            // Check for mouse input
-            if (Input.GetButtonDown("Fire1"))
-            {
-                ComboSystem();
- 
-            }
+            nextFireTime = Time.time + cooldownTime;
         }
     }
- 
+
     void ComboSystem()
     {
         //so it looks at how many clicks have been made and if one animation has finished playing starts another one.
@@ -59,18 +62,21 @@ public class ComboAttack : MonoBehaviour
         noOfClicks++;
         if (noOfClicks == 1)
         {
+            Debug.Log("hit1");
             anim.SetBool("hit1", true);
         }
         noOfClicks = Mathf.Clamp(noOfClicks, 0, 3);
- 
+
         if (noOfClicks >= 2  && anim.GetCurrentAnimatorStateInfo(0).IsName("melee1"))
         {
+            Debug.Log("hit2");
             anim.SetBool("hit1", false);
             anim.SetBool("hit2", true);
             rb.AddForce(new Vector2(0, 1200));
         }
         if (noOfClicks >= 3 && anim.GetCurrentAnimatorStateInfo(0).IsName("melee2"))
         {
+            Debug.Log("hit3");
             anim.SetBool("hit2", false);
             anim.SetBool("hit3", true);
             rb.velocity = new Vector2(1900, rb.velocity.y);
