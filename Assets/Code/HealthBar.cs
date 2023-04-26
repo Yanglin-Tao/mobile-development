8d96a3cdd3edd54f47510c3d89f752956558e631
Suning Yao <sy2940@nyu.edu>
2023-04-26 13:00:02 -0400

    Test health bar

--- Assets/Code/HealthBar.cs
+++ Assets/Code/HealthBar.cs
@@ -7,6 +7,16 @@ public class HealthBar : MonoBehaviour
 {
     public Slider slider;
 
+    private void Update() {
+        // Testing only
+        // if space key is pressed, add one to slider's value
+        if (Input.GetKeyDown(KeyCode.Space)){
+            // subtract slider value by random value from 1 to 30
+            slider.value -= Random.Range(1, 30);
+            Debug.Log(slider.value);
+        }
+    }
+
     public void SetMaxHealth(int health){
         slider.maxValue = health;
         slider.value = health;
