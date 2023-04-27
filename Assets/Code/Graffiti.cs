b0f13509262a9428be62bb90c662d57f6b78bf54
Suning Yao <sy2940@nyu.edu>
2023-04-26 22:03:09 -0400

    Manually restore changes, please do not revert

--- Assets/Code/Graffiti.cs
+++ Assets/Code/Graffiti.cs
@@ -52,7 +52,7 @@ public class Graffiti : MonoBehaviour
     }
 
     void Update(){
-        _gameManager.SetLife(health);
+        _gameManager.SetHealth(health);
         if (Input.GetButtonDown("Jump") && isGrounded){
             rb.AddForce(new Vector2(0, jumpForce));
             // play jump sound
@@ -62,7 +62,7 @@ public class Graffiti : MonoBehaviour
         if (Input.GetButtonDown("Fire2") && (Attacks.Length != 0)){
             Animator.SetBool("ULT", true);
             GameObject newBullet;
-        
+
             if (specialCharge == 0){
                 newBullet = Instantiate(Attacks[current], shootPosition.position, Quaternion.identity);
                 newBullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(bulletSpeed, 0));
