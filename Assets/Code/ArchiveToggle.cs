45e78a18b27c4ae555d31082c72951fb1c2a842c
Yanglin Tao <yt2061@nyu.edu>
2023-05-04 17:30:12 -0400

    Archive mode added

---
+++ Assets/Code/ArchiveToggle.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArchiveToggle : MonoBehaviour
+{
+    public Toggle archiveToggle; // Reference to the Toggle UI element
+    GameManager _gameManager;
+
+    private void Start()
+    {
+        _gameManager = GameObject.FindObjectOfType<GameManager>();
+        archiveToggle.onValueChanged.AddListener(OnToggleValueChanged); // Register a listener for when the toggle value changes
+    }
+
+    // enter the archive mode
+    private void OnToggleValueChanged(bool isOn)
+    {
+        if (isOn)
+        {
+            // unlock all levels
+            _gameManager.unlockLevel("Level2");
+            _gameManager.unlockLevel("Level3");
+            _gameManager.unlockLevel("Level4");
+        }
+    }
+}
